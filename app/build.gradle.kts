1fe70e29e3926ac825b16049074c5eb260c2e46a
Aline Sayuri Kadekaro <aline.ask@gmail.com>
2024-06-06 03:58:21 -0300

    add mapa api google maps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -37,11 +38,17 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
+        viewBinding = true
         compose = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
+
+    secrets {
+        propertiesFileName = "local.properties"
+    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -62,6 +69,21 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.maps.android:maps-utils-ktx:5.0.0")
+    implementation("com.google.maps.android:maps-ktx:5.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    // Google Maps Compose library
+    val mapsComposeVersion = "4.4.1"
+    implementation("com.google.maps.android:maps-compose:$mapsComposeVersion")
+    // Google Maps Compose utility library
+    implementation("com.google.maps.android:maps-compose-utils:$mapsComposeVersion")
+    // Google Maps Compose widgets library
+    implementation("com.google.maps.android:maps-compose-widgets:$mapsComposeVersion")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/br/com/oceanguardian/TelaMapa.kt
+++ app/src/main/java/br/com/oceanguardian/TelaMapa.kt
@@ -15,18 +15,49 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class TelaMapa : AppCompatActivity() {
+class TelaMapa : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mGoogleMap:GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tela_mapa)
 
-
+//botão voltar
         val btnvoltar: ImageButton = findViewById(R.id.btn_voltar)
 
         btnvoltar.setOnClickListener {
             val intent = Intent(this, TelaInicio::class.java)
             startActivity(intent)
         }
+
+//mapa
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
     }
-}
+
+//pin
+
+        override fun onMapReady(googleMap: GoogleMap) {
+            val sydney = LatLng(-33.852, 151.211)
+            googleMap.addMarker(
+                MarkerOptions()
+                    .position(sydney)
+                    .title("Marker in Sydney"))
+
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        }
+    }
+
+    fun onMapReady(mGoogleMap: GoogleMap) {
+    }
+
+
+
